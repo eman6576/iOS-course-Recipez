b721390c02ee3e4a70e13400de32c7cfe9ba209d
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-11 16:54:49 -0500

    Created a subclass from the data model for a recipe.

---
+++ Recipe.swift
@@ -0,0 +1,26 @@
+//
+//  Recipe.swift
+//  Recipez
+//
+//  Created by Emanuel  Guerrero on 3/11/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+class Recipe: NSManagedObject {
+    func setRecipeImage(image: UIImage) {
+        let data = UIImagePNGRepresentation(image)
+        
+        self.image = data
+    }
+    
+    func getRecipeImage() -> UIImage {
+        let image = UIImage(data: self.image!)!
+        
+        return image
+    }
+}
