4989eb6b01e04e3f6b1f2b4a79679c853f4df6d5
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-11 18:18:51 -0500

    Did code clean up.

--- Recipez/RecipeCell.swift
+++ Recipez/RecipeCell.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class RecipeCell: UITableViewCell {
+    
     @IBOutlet weak var recipeTitle: UILabel!
     @IBOutlet weak var recipeImage: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     func configureCell(recipe: Recipe) {
         recipeTitle.text = recipe.title
-        
         recipeImage.image = recipe.getRecipeImage()
     }
 }
